d7c03ebeeda6e1f9e53594578a4f70e0b04c4329
Yan Dekov <Air262921@gmail.com>
2024-08-27 16:50:14 +0300

    fix

--- src/domain/Specifications/Chat/SortChatSpec.cs
+++ src/domain/Specifications/Chat/SortChatSpec.cs
@@ -6,12 +6,18 @@ namespace domain.Specifications.Chat;
 
 public class SortChatSpec : SortCollectionSpec<ChatModel>
 {
-    public SortChatSpec(int skip, int count, bool byDesc) 
+    public SortChatSpec(int skip, int count, bool byDesc, int userId) 
         : base(skip, count, byDesc, x => x.CreatedAt)
     {
+        UserId = userId;
+
+        Query.Where(x => x.CandidateId.Equals(userId) || x.EmployerId.Equals(UserId));
+
         Query.OrderByDescending(x => x.Messages != null && x.Messages.Any(m => !m.IsRead))
                      .ThenByDescending(x => x.CreatedAt);
 
         Query.Skip(skip).Take(count);
     }
+
+    public int UserId { get; private set; }
 }
--- src/domain/Specifications/Message/SortMessageSpec.cs
+++ src/domain/Specifications/Message/SortMessageSpec.cs
@@ -7,9 +7,15 @@ namespace domain.Specifications.Message;
 
 public class SortMessageSpec : SortCollectionSpec<MessageModel>
 {
-    public SortMessageSpec(int skip, int count, bool byDesc)
+    public SortMessageSpec(int skip, int count, bool byDesc, int chatId, int userId)
         : base(skip, count, byDesc, x => x.SentAt)
     {
+        ChatId = chatId;
+        UserId = userId;
+
+        Query.Where(x => x.ChatId.Equals(ChatId));
+        Query.Where(x => x.CandidateId.Equals(userId) || x.EmployerId.Equals(UserId));
+
         if (OnlyRead.HasValue)
             Query.Where(x => x.IsRead.Equals(OnlyRead));
 
@@ -22,6 +28,8 @@ public class SortMessageSpec : SortCollectionSpec<MessageModel>
         Initialize();
     }
 
+    public int ChatId { get; set; }
+    public int UserId { get; set; }
     public bool? OnlyRead { get; set; }
     public string? KeyWord { get; set; }
 }
