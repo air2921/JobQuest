d6b8cb0cf15e684862c7658f9f79dfa86b36634d
Yan Dekov <Air262921@gmail.com>
2024-08-30 04:12:33 +0300

    wip

--- src/application/Workflows/Auth/LogoutWk.cs
+++ src/application/Workflows/Auth/LogoutWk.cs
@@ -4,6 +4,7 @@ using domain.Localize;
 using domain.Models;
 using domain.Specifications.Auth;
 using JsonLocalizer;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace application.Workflows.Auth;
@@ -12,7 +13,7 @@ public class LogoutWk(
     IRepository<AuthModel> repository,
     ILocalizer localizer) : Responder
 {
-    public async Task<Response> Logout(string refresh)
+    public async Task<Response> Logout(string refresh, bool clearAllSessions = false)
     {
         try
         {
@@ -20,11 +21,23 @@ public class LogoutWk(
             if (model is null)
                 return Response(404, localizer.Translate(Messages.NOT_FOUND));
 
+            if (clearAllSessions)
+                await LogoutAllSessions(model.UserId);
+
             return Response(404);
         }
         catch (EntityException ex)
         {
             return Response(500, ex.Message);
         }
+    }
+
+    private async Task LogoutAllSessions(int userId)
+    {
+        var count = repository.GetCount(new CountAuthSpec { UserId = userId });
+        var modelIdentifiers = (await repository.GetRangeAsync(new SortAuthSpec(0, count, true) { UserId = userId }))
+            .Select(x => x.TokenId).ToArray();
+
+        await repository.DeleteRangeAsync(modelIdentifiers);
     }
 }
