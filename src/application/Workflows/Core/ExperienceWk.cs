aebd5a37bffbecc3299d7f866502084c9c32aa59
Yan Dekov <Air262921@gmail.com>
2024-08-30 03:29:41 +0300

    wip

---
+++ src/application/Workflows/Core/ExperienceWk.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using common.DTO.ModelDTO;
+using common.Exceptions;
+using domain.Abstractions;
+using domain.Localize;
+using domain.Models;
+using domain.SpecDTO;
+using JsonLocalizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.Workflows.Core;
+
+public class ExperienceWk(
+    IRepository<ExperienceModel> repository,
+    IDatabaseTransaction databaseTransaction,
+    ILocalizer localizer,
+    IMapper mapper) : Responder
+{
+    public async Task<Response> GetRange(SortExperienceDTO dto)
+    {
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> GetSingle(int id)
+    {
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> RemoveSingle(int id, int userId)
+    {
+        using var transaction = databaseTransaction.Begin();
+
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            transaction.Rollback();
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> RemoveRange(IEnumerable<int> identifiers, int userId)
+    {
+        using var transaction = databaseTransaction.Begin();
+
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            transaction.Rollback();
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> AddSingle(VacancyDTO dto)
+    {
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> AddRange(IEnumerable<ExperienceDTO> dtos)
+    {
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            return Response(500, ex.Message);
+        }
+    }
+
+    public async Task<Response> Update(ExperienceDTO dto, int userId)
+    {
+        try
+        {
+
+        }
+        catch (EntityException ex)
+        {
+            return Response(500, ex.Message);
+        }
+    }
+}
