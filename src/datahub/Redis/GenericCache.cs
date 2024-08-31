ef4f496fc5cd6f1ceffcca3d3fe5248816e42ec2
Yan Dekov <Air262921@gmail.com>
2024-08-31 07:15:16 +0300

    fixes

--- src/datahub/Redis/GenericCache.cs
+++ src/datahub/Redis/GenericCache.cs
@@ -7,31 +7,31 @@ namespace datahub.Redis;
 
 public class GenericCache<T>(IDataCache<ConnectionPrimary> dataCache) : IGenericCache<T> where T : class
 {
-    public async Task<IEnumerable<T>?> GetRangeAsync(string key, Func<Task<IEnumerable<T>>> dataRetrievalCallback, int expirationMin = 10)
+    public async Task<IEnumerable<T>?> GetRangeAsync(string key, Func<Task<IEnumerable<T>>> dataReceiverCallback, int expirationMin = 10)
     {
         var cache = await dataCache.GetRangeAsync<T>(key);
         if (cache is not null)
             return cache;
 
-        var dbData = await dataRetrievalCallback();
-        if (dbData is null)
+        var data = await dataReceiverCallback();
+        if (data is null)
             return null;
 
-        await dataCache.SetAsync(key, dbData, TimeSpan.FromMinutes(expirationMin));
-        return dbData;
+        await dataCache.SetAsync(key, data, TimeSpan.FromMinutes(expirationMin));
+        return data;
     }
 
-    public async Task<T?> GetSingleAsync(string key, Func<Task<T?>> dataRetrievalCallback, int expirationMin = 10)
+    public async Task<T?> GetSingleAsync(string key, Func<Task<T?>> dataReceiverCallback, int expirationMin = 10)
     {
         var cache = await dataCache.GetSingleAsync<T>(key);
         if (cache is not null)
             return cache;
 
-        var dbData = await dataRetrievalCallback();
-        if (dbData is null)
+        var data = await dataReceiverCallback();
+        if (data is null)
             return null;
 
-        await dataCache.SetAsync(key, dbData, TimeSpan.FromMinutes(expirationMin));
-        return dbData;
+        await dataCache.SetAsync(key, data, TimeSpan.FromMinutes(expirationMin));
+        return data;
     }
 }
--- src/domain/Abstractions/IGenericCache.cs
+++ src/domain/Abstractions/IGenericCache.cs
@@ -6,6 +6,6 @@ namespace domain.Abstractions;
 
 public interface IGenericCache<T> where T : class
 {
-    Task<IEnumerable<T>?> GetRangeAsync(string key, Func<Task<IEnumerable<T>>> dataRetrievalCallback, int expirationMin = 10);
-    Task<T?> GetSingleAsync(string key, Func<Task<T?>> dataRetrievalCallback, int expirationMin = 10);
+    Task<IEnumerable<T>?> GetRangeAsync(string key, Func<Task<IEnumerable<T>>> dataReceiverCallback, int expirationMin = 10);
+    Task<T?> GetSingleAsync(string key, Func<Task<T?>> dataReceiverCallback, int expirationMin = 10);
 }
